0509cd6768f1c3deca6c2f29d9dfa39d70183b3f
Oleg K <tup1k@yandex.ru>
2024-07-20 23:37:39 +0300

    Выполнен окончательный рефакторинг под MVP и проведен тест функции convert

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,75 +1,19 @@
 import UIKit
 
-final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate, AlertPresenterDelegate {
+final class MovieQuizViewController: UIViewController, AlertPresenterDelegate, MovieQuizViewControllerProtocol {
     
     @IBOutlet private weak var questionTitleLabel: UILabel! // Статичный тайтл ВОПРОС
     @IBOutlet private weak var indexLabel: UILabel! // Тайтл с номером вопроса из числа всех вопросов (1/10)
     @IBOutlet private weak var imageView: UIImageView! // Тайтл с картинкой
-    @IBOutlet private weak var questionLabel: UILabel! // Тайтл с текстом вопроса квиза (в нашем случае грузится из моковских данных)
+    @IBOutlet private weak var questionLabel: UILabel! // Тайтл с текстом вопроса квиза
     @IBOutlet private weak var yesButton: UIButton! // Дизайн кнопки ДА
     @IBOutlet private weak var noButton: UIButton! // Дизайн кнопки НЕТ
     @IBOutlet private weak var activityIndicator: UIActivityIndicatorView! // Индикатор загрузки
     
-    
-    private var questionFactory: QuestionFactoryProtocol? //  Фабрика вопросов
     private var alertPresenter: AlertPresenterProtocol?  //  Всплывающее окно
     private var statisticService: StatisticServiceProtocol = StatisticService() // Статистика по всем квизам
-    private var correctAnswers: Int = 0 // Переменная число правильных ответов для вывода в конце
     private var sp05CurrentQuestion: QuizQuestion? // Вопрос который видит пользователь
-    private let presenter = MovieQuizPresenter()
-    
-    // Функция выключает/выключает кнопки на время загрузки нового вопроса
-    private func buttonsOnOff(turn status: Bool) {
-        yesButton.isEnabled = status
-        noButton.isEnabled = status
-    }
-    
-    func didLoadDataFromServer() {
-        activityIndicator.isHidden = true
-        questionFactory?.requestNextQuestion()
-    }
-
-    // Метод, сообщающий об ошибке загрузки данных с сервера
-    func didFailToLoadData(with error: Error) {
-        showNetworkError(message: error.localizedDescription)
-    }
-    
-    // Функция параметров индикатора загрузки при его включении
-    private func showLoadingIndicator() {
-        activityIndicator.isHidden = false // показываем индикатор загрузки
-        activityIndicator.startAnimating() // включаем анимацию
-    }
-    
-    // Функция параметров индикатора загрузки при его выключении
-    private func hideLoadingIndicator() {
-        activityIndicator.isHidden = true // показываем индикатор загрузки
-        activityIndicator.stopAnimating() // включаем анимацию
-    }
-    
-    // Метод вызова всплывающего окна при ошибке загрузки данных
-    private func showNetworkError(message: String) {
-        hideLoadingIndicator() // скрываем индикатор загрузки
-       
-        let errorModel = AlertModel(
-            title: "Ошибка",
-            message: message,
-            buttonText: "Попробовать ещё раз") { [weak self] in
-                guard let self = self else {return}
-                self.presenter.resetQuestionIndex()
-                self.correctAnswers = 0
-                //self.questionFactory?.requestNextQuestion()
-                self.questionFactory?.loadData()
-            }
-        alertPresenter = AlertPresenter(delegate: self)
-        alertPresenter?.newLogicShowRez(newQuiz: errorModel)
-    }
-    
-    // Метод обнуления параметров при запуске нового квиза
-    private func newQuizData() {
-        presenter.resetQuestionIndex()
-        correctAnswers = 0
-        questionFactory?.requestNextQuestion()
-    }
+    private var presenter: MovieQuizPresenter!
     
     // Функция, созданная для описания шрифтов кодом
     private func setFontProperties() {
@@ -78,6 +22,55 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate, 
         questionLabel.font = UIFont(name: "YSDisplay-Bold", size: 23)
         yesButton.titleLabel?.font = UIFont(name: "YSDisplay-Medium", size: 20)
         noButton.titleLabel?.font = UIFont(name: "YSDisplay-Medium", size: 20)
+    }
+    
+    // Функция выключает/выключает кнопки на время загрузки нового вопроса
+    private func buttonsOnOff(turn status: Bool) {
+        yesButton.isEnabled = status
+        noButton.isEnabled = status
+    }
+    
+    // Функция параметров индикатора загрузки при его включении
+    func showLoadingIndicator() {
+        activityIndicator.isHidden = false // показываем индикатор загрузки
+        activityIndicator.startAnimating() // включаем анимацию
+    }
+    
+    // Функция параметров индикатора загрузки при его выключении
+    func hideLoadingIndicator() {
+        activityIndicator.isHidden = true // показываем индикатор загрузки
+        activityIndicator.stopAnimating() // включаем анимацию
+    }
+    
+    // Функция прозрачности рамки вокруг картинки
+    func clearImageBorder() {
+        imageView.layer.borderColor = UIColor.clear.cgColor
+    }
+    
+    // Метод вызова всплывающего окна при ошибке загрузки данных
+    func showNetworkError(message: String) {
+        hideLoadingIndicator() // скрываем индикатор загрузки
+        
+        let errorModel = AlertModel(
+            title: "Ошибка",
+            message: message,
+            buttonText: "Попробовать ещё раз") { [weak self] in
+                guard let self = self else {return}
+                self.presenter.resetGame()
+                self.presenter.questionfactoryLoadData()
+            }
+        alertPresenter = AlertPresenter(delegate: self)
+        alertPresenter?.newLogicShowRez(newQuiz: errorModel)
+    }
+    
+    // Функция подсветки рамок для правильного/неправильного ответа
+    func highlightImageBorder(isCorrectAnswer: Bool) {
+        imageView.layer.masksToBounds = true // даём разрешение на рисование рамки
+        imageView.layer.borderWidth = 8 // толщина рамки
+        imageView.layer.cornerRadius = 20
+        buttonsOnOff(turn: true)
+        imageView.layer.borderColor =  isCorrectAnswer ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
+        buttonsOnOff(turn: false)
     }
     
     // Метод вывода на экран приложения конвертированных данных
@@ -89,72 +82,26 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate, 
     }
     
     // Метод выводит на экран результаты квиза ну и обнуляет все результаты при нажатии на кнопку
-    private func showRes(quiz result: AlertModel) {
+    func showRes(quiz result: AlertModel) {
         alertPresenter = AlertPresenter(delegate: self)
         alertPresenter?.newLogicShowRez(newQuiz: result)
     }
-    
-    // Метод выполняет действия в случае если ответ верный/не верный
-    func showAnswerResult(isCorrect: Bool) {
-        imageView.layer.masksToBounds = true // даём разрешение на рисование рамки
-        imageView.layer.borderWidth = 8 // толщина рамки
-        imageView.layer.cornerRadius = 20
-        buttonsOnOff(turn: true)
-        imageView.layer.borderColor =  isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
-        buttonsOnOff(turn: false)
         
-        if isCorrect {
-            correctAnswers += 1
-        }
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        presenter = MovieQuizPresenter(viewController: self) // создаем ссылку на viewcontroller для presenter
+        setFontProperties() // Создаем формат шрифтов для текста/кнопок
+    }
         
-        // Отложенный запуск метода показа следуюшего вопроса/окончания квиза через 1 с
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { [weak self] in
-            guard let self = self else { return }
-            self.showNextQuestionOrResults()
-        }
-    }
-    
-    // Метод либо показывает следующий вопрос, либо показывает экран результатов квиза
-    private func showNextQuestionOrResults() {
-        if presenter.isLastQuestion() {
-            statisticService.store(correct: correctAnswers, total: presenter.questionsAmount)
-            let text = "Ваш результат: \(correctAnswers)/\(presenter.questionsAmount)\n Количество сыграных квизов: \(statisticService.gamesCount) \n Рекорд: \(statisticService.bestGame.correct)/\(statisticService.bestGame.total) (\(statisticService.bestGame.date.dateTimeString))\n Средняя точность: \(String(format: "%.2f", statisticService.totalAccuracy)) %" // Выводит общую статистику по всем квизам
-            let viewModel = AlertModel( // Создает объект структуры финала квиза
-                title: "Этот раунд окончен!",
-                message: text,
-                buttonText: "Сыграть ещё раз",
-                callback: newQuizData )
-            showRes(quiz: viewModel) // Запускаем метод вывода на экран заключительного экрана
-            imageView.layer.borderColor = UIColor.clear.cgColor // Почему то в учебе не указано что цвет рамки надо отключать или я слепой
-        } else { // 2
-            presenter.switchToNextQuestion() // Перебираем следующий индекс-вопрос
-            self.questionFactory?.requestNextQuestion()
-            imageView.layer.borderColor = UIColor.clear.cgColor // Почему то в учебе не указано что цвет рамки надо отключать или я слепой
-        }
-    }
-    
-    // Экшн кнопки ДА
+    // Экшн кнопки ДА - ссылка на presenter
     @IBAction private func yesButtonClicked(_ sender: Any) {
         presenter.yesButtonClicked()
     }
     
-    // Экшн кнопки НЕТ
+    // Экшн кнопки НЕТ - ссылка на presenter
     @IBAction private func noButtonClicked(_ sender: Any) {
         presenter.noButtonClicked()
-    }
-    
-    
-    // MARK: - Lifecycle
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        setFontProperties() //  Формат шрифтов
-        
-        questionFactory = QuestionFactory(moviesLoader: MoviesLoader(), delegate: self)
-        showLoadingIndicator()
-        questionFactory?.loadData()
-        
-        presenter.viewController = self
     }
     
     //MARK: - QuestionFactoryDelegate
